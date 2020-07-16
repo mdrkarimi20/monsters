7c4b6b9bab151e2dfca845a81d2fdcf7e8626afb
mdrkarimi20 <mdrkarimi20@gmail.com>
2020-07-16 16:50:26 +0430

    Updates

---
+++ precache-manifest.38c182948484a07c9048b0b0c68e7d21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90b0f587f37a32198837edf174929ea4",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "cfd7de6a82d8bbaa435f",
+    "url": "/monsters/static/css/main.72e045f2.chunk.css"
+  },
+  {
+    "revision": "f2bbe547c5de1444685e",
+    "url": "/monsters/static/js/2.76a7909c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.76a7909c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfd7de6a82d8bbaa435f",
+    "url": "/monsters/static/js/main.cd8307e7.chunk.js"
+  },
+  {
+    "revision": "b9eb6b362cf5c12b05d7",
+    "url": "/monsters/static/js/runtime-main.5ccbf1f2.js"
+  }
+]);
